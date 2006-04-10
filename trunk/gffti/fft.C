07ea5a78aabb195a1cc995ed4df2cbd0d6222623
mbaptist <mbaptist@37bf1541-a92c-0410-ab0e-67408351716b>
2006-04-10 16:30:03 +0000

    Update 10-04-2006 17:30

--- trunk/gffti/fft.C
+++ trunk/gffti/fft.C
@@ -6,80 +6,142 @@ namespace goops
 
 //Generic Ctor
 template <class RealType,class FourierType>
-FFT<RealType,FourierType>::FFT():
-realdata(0),
-fourierdata(0)
-{
-}
-
-//Generic Dtor
-template <class RealType,class FourierType>
-FFT<RealType,FourierType>::~FFT()
+FFT_BASE<RealType,FourierType>::FFT_BASE()
 {
 	realdata=0;
 	fourierdata=0;
 }
 
-//Generic direct transform
+//Generic Dtor
 template <class RealType,class FourierType>
-void FFT<RealType,FourierType>::direct_transform(FourierType & fourierfield,RealType & realfield)
+FFT_BASE<RealType,FourierType>::~FFT_BASE()
 {
-	if(realdata!=realfield.data()||fourierdata!=fourierfield.data())
-	{
-		destroy_plans();
-		realdata=realfield.data();
-		fourierdata=fourierfield.data();
-		size=fourierfield.size();
-		create_plans();
-	}
-	fftw_execute(direct_plan);
-	fourierfield/=size;
-}
-
-//Generic inverse transform
-template <class RealType,class FourierType>
-void FFT<RealType,FourierType>::inverse_transform(RealType & realfield,FourierType & fourierfield)
-{
-	if(realdata!=realfield.data()||fourierdata!=fourierfield.data())
-	{
-		destroy_plans();
-		realdata=realfield.data();
-		fourierdata=fourierfield.data();
-		size=fourierfield.size();
-		create_plans();
-	}
-	fftw_execute(inverse_plan);
-	realfield/=size;
+	realdata=0;
+	fourierdata=0;
 }
 
 //Generic create plans
 template <class RealType,class FourierType>
-	void FFT<RealType,FourierType>::create_plans()
+	void FFT_BASE<RealType,FourierType>::create_plans()
 {
+	do_create_plans();
+	plans_created=1;
 }
 
 //Generic destroy plans
 template <class RealType,class FourierType>
-	void FFT<RealType,FourierType>::destroy_plans()
+	void FFT_BASE<RealType,FourierType>::destroy_plans()
 {
-	fftw_destroy_plan(direct_plan);
-	fftw_destroy_plan(inverse_plan);
+	if(plans_created)
+	{
+		fftw_destroy_plan(direct_plan);
+		fftw_destroy_plan(inverse_plan);
+	}
+	plans_created=0;	
 }
 
+//Generic direct transform
+template <class RealType,class FourierType>
+void FFT_BASE<RealType,FourierType>::direct_transform(FourierType & fourierfield,RealType & realfield)
+{
+	switch_data(realfield,fourierfield);
+	fftw_execute(direct_plan);
+	fourierfield/=realsize;
+}
 
+//Generic inverse transform
+template <class RealType,class FourierType>
+void FFT_BASE<RealType,FourierType>::inverse_transform(RealType & realfield,FourierType & fourierfield)
+{
+	switch_data(realfield,fourierfield);
+	fftw_execute(inverse_plan);
+	realfield/=fouriersize;
+}
+
+//Generic inverse transform
+template <class RealType,class FourierType>
+	void FFT_BASE<RealType,FourierType>::switch_data(RealType & realfield,FourierType & fourierfield)
+{
+	if(realdata!=realfield.data()||fourierdata!=reinterpret_cast<FDT*>(fourierfield.data()))
+	{
+		destroy_plans();
+		realdata=realfield.data();
+		fourierdata=reinterpret_cast<FDT*>(fourierfield.data());
+		realsize=realfield.size();
+		fouriersize=fourierfield.size();
+		create_plans();
+	}
+}
 
 //Specialization
 
 
-template <int D>
-	//template <>
-	void FFT<cat::array<RS,D>,cat::array<CS,D> >::create_plans()
+template <>
+	template <int D>
+	void FFT<cat::array<CS,D>,cat::array<CS,D> >::do_create_plans()
 {
-	direct_plan=fftw_plan_dft_r2c_1d(&size,realdata,fourierdata,FFTW_ESTIMATE);
-	inverse_plan=fftw_plan_dft_c2r_1d(&size,fourierdata,realdata,FFTW_ESTIMATE);
+	direct_plan=fftw_plan_dft(D,&realsize,realdata,fourierdata,FFTW_FORWARD,FFTW_ESTIMATE);
+	inverse_plan=fftw_plan_dft(D,&fouriersize,fourierdata,realdata,FFTW_FORWARD,FFTW_ESTIMATE);
+}
+
+
+template <>
+	template <int D>
+	void FFT<cat::array<RS,D>,cat::array<CS,D> >::do_create_plans()
+{
+	direct_plan=fftw_plan_dft_r2c(D,&realsize,realdata,fourierdata,FFTW_ESTIMATE);
+	inverse_plan=fftw_plan_dft_c2r(D,&fouriersize,fourierdata,realdata,FFTW_ESTIMATE);
 }
 
 
 
+
+template <>
+	template <int D>
+	FFT<cat::array<RS,D>,cat::array<RS,D> >::FFT(const string & subtype):
+	FFT_BASE<cat::array<RS,D>,cat::array<RS,D> >()
+{
+	if(subtype=="sin")
+	{
+		r2r_kind_direct=FFTW_RODFT00;
+		r2r_kind_inverse=FFTW_RODFT00;
+		//norm_factor_direct=(fouriersize+1)
+	}
+	else if(subtype=="cos")
+	{
+		r2r_kind_direct=FFTW_REDFT00;
+		r2r_kind_inverse=FFTW_REDFT00;
+	}
+	else
+	{
+		cout << "Undefined subtype! Aborting..." << endl;
+	}
+}
+
+template <>
+	template <int D>
+	void FFT<cat::array<RS,D>,cat::array<RS,D> >::do_create_plans()
+{
+	direct_plan=fftw_plan_r2r(D,&realsize,realdata-1,fourierdata,&r2r_kind_direct,FFTW_ESTIMATE);
+	inverse_plan=fftw_plan_r2r(D,&fouriersize,fourierdata,realdata+1,&r2r_kind_inverse,FFTW_ESTIMATE);
+}
+
+// template <>
+// 	template <int D>
+// 	void FFT<cat::array<RS,D>,cat::array<RS,D> >::direct_transform(FourierType & fourierfield,RealType & realfield)
+// {
+// 	switch_data(realfield,fourierfield);
+// 	fftw_execute(direct_plan);
+// 	fourierfield/=realsize;
+// }
+// template <>
+// 	template <int D>
+// 	void FFT<cat::array<RS,D>,cat::array<RS,D> >::inverse_transform(RealType & realfield,FourierType & fourierfield)
+// {
+// 	switch_data(realfield,fourierfield);
+// 	fftw_execute(direct_plan);
+// 	fourierfield/=realsize;
+// }
+	
 
 }
