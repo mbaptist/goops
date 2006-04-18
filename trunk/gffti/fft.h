5d12a56606311d64e860637ef907e7a82a2267df
mbaptist <mbaptist@37bf1541-a92c-0410-ab0e-67408351716b>
2006-04-18 16:30:03 +0000

    Update 18-04-2006 17:30

--- trunk/gffti/fft.h
+++ trunk/gffti/fft.h
@@ -22,7 +22,7 @@
 
 #include <cmath>
 
-
+//Generic FFT class
 template <class RealType,class FourierType>
 class FFT
 {
@@ -31,7 +31,6 @@ public:
 	Plan<RealType,const FourierType> inverse_plan;
 	//Ctors
 	FFT();//Default Ctor
-	FFT(const string &);
 	//Dtor
 	~FFT();
 private:
@@ -45,44 +44,57 @@ public:
 	//RealType inverse_transform(const FourierType & fourierfield);
 };
 
+template <>
+template <int D>
+class FFT<cat::array<CS,D>,cat::array<CS,D> >
+{
+	typedef cat::array<CS,D> RealType;
+	typedef cat::array<CS,D> FourierType;
+public:
+	Plan<FourierType,const RealType> direct_plan;
+	Plan<RealType,const FourierType> inverse_plan;
+	//Ctors
+	FFT();//Default Ctor
+	//Dtor
+	~FFT();
+private:
+	//Forbidden Ctors
+	FFT(const FFT &);//Copy ctor
+public:
+	//Public methods
+	void direct_transform(FourierType & fourierfield,const RealType & realfield);
+	void inverse_transform(RealType & realfield,const FourierType & fourierfield);
+	//FourierType direct_transform(const RealType & realfield);
+	//RealType inverse_transform(const FourierType & fourierfield);
+};
 
-
-
-
-
-// template <class RealType,class FourierType>
-// class FFT:public FFT_BASE<RealType,FourierType>
-// {
-// 	using FFT_BASE<RealType,FourierType>::direct_plan;
-// 	using FFT_BASE<RealType,FourierType>::inverse_plan;
-// };
-// 
-// template <>
-// template<int D>
-// class FFT<cat::array<CS,D>,cat::array<CS,D> >:public FFT_BASE<cat::array<CS,D>,cat::array<CS,D> >
-// {
-// 	using FFT_BASE<cat::array<CS,D>,cat::array<CS,D> >::direct_plan;
-// 	using FFT_BASE<cat::array<CS,D>,cat::array<CS,D> >::inverse_plan;
-// public:
-// 	FFT();	
-// private:
-// 	FFT(const FFT &);
-// };
-// 
-// template <>
-// class FFT<cat::array<RS,1>,cat::array<RS,1> >:public FFT_BASE<cat::array<RS,1>,cat::array<RS,1> >
-// {
-// 	using FFT_BASE<cat::array<RS,1>,cat::array<RS,1> >::direct_plan;
-// 	using FFT_BASE<cat::array<RS,1>,cat::array<RS,1> >::inverse_plan;
-// public:
-// 	FFT(const string & subtype);
-// private:
-// 	FFT();
-// 	FFT(const FFT &);
-// public:
-// 	void direct_transform(cat::array<RS,1> & fourierfield, const cat::array<RS,1> & realfield);
-// 	void inverse_transform(cat::array<RS,1> & realfield,const cat::array<RS,1> & fourierfield);
-// };
+template <>
+class FFT<cat::array<RS,1>,cat::array<RS,1> >
+{
+	typedef cat::array<RS,1> RealType;
+	typedef cat::array<RS,1> FourierType;
+public:
+	Plan<FourierType,const RealType> direct_plan;
+	Plan<RealType,const FourierType> inverse_plan;
+	//Ctors
+	FFT(const string & subtype__);//Default Ctor
+	//Dtor
+	~FFT();
+private:
+	//Forbidden Ctors
+	FFT();//Default Ctor
+	FFT(const FFT &);//Copy ctor
+public:
+	//Public methods
+	void direct_transform(FourierType & fourierfield,const RealType & realfield);
+	void inverse_transform(RealType & realfield,const FourierType & fourierfield);
+	//FourierType direct_transform(const RealType & realfield);
+	//RealType inverse_transform(const FourierType & fourierfield);
+private:
+	const string subtype;
+	fftw_r2r_kind r2r_direct_kind();
+	fftw_r2r_kind r2r_inverse_kind();
+};
 
 #include "fft.C"
 
