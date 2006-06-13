cf0145c7a8478b24ccd4862a37126fbff64ca31a
mbaptist <mbaptist@37bf1541-a92c-0410-ab0e-67408351716b>
2006-06-13 15:01:01 +0000

    code factorising;corrected sin/cos ffts;code cleanup

--- spectral/spectral_fourier.C
+++ spectral/spectral_fourier.C
@@ -4,354 +4,231 @@
 #include "globals.h"
 
 #include "../goops_types.h"
-#include "../gffti/gffti.h"
+#include "../fft/fft.h"
 
 #include <cat.h>
-
-#include <iomanip>
-
-#include <complex>
-#include <fstream>
 
 using namespace std;
 using namespace cat;
 
 //Construtor
-spectral_fourier::spectral_fourier(const int & n1__,
-				   const int & n2__,
-				   const int & n3__,
-				   const Real & l1__,
-				   const Real & l2__,
-				   const Real & l3__):
-  n1(n1__),
-  n2(n2__),
-  n3(n3__),
-  l1(l1__),
-  l2(l2__),
-  l3(l3__),
-  wv(n1,n2,n3/2+1),
-  wv2(n1,n2,n3/2+1),
-  dealiasing_limit(0),
-  dealiasing_mask(n1,n2,n3/2+1),
-  fft(cat::tvector<int,3>(n1,n2,n3)),
-  sfft(cat::tvector<int,3>(n1,n2,n3))
+SpectralFourier::SpectralFourier(const int & n1__,const int & n2__,const int & n3__,
+                                 const Real & l1__,const Real & l2__,const Real & l3__):
+SpectralFourierBase(n1__,n2__,n3__,
+                    n1__,n2__,n3__/2+1,
+                    l1__,l2__,l3__,
+                    2*M_PI/l1__,2*M_PI/l2__,M_PI/l3__),
+fft(cat::tvector<int,3>(n1,n2,n3)),
+sfft()
 {
+	initialise();
+}
 
-  //define aspect ratios
-  Real ar1=2*M_PI/l1;
-  Real ar2=2*M_PI/l2;
-  Real ar3=2*M_PI/l3;
-  
+void SpectralFourier::initialise()
+{
   //evaluate wave vectors
-  for (int k=0;k<n3/2+1;++k)
-    {
-      for (int j=0;j<n2/2+1;++j)
+	for (int k=0;k<n3/2+1;++k)
 	{
-	  for (int i=0;i<n1/2+1;++i)
-	    wv(i,j,k)=cat::tvector<Real,3>(ar1*i,ar2*j,ar3*k);
-	  for (int i=n1/2+1;i<n1;++i)
-	    wv(i,j,k)=cat::tvector<Real,3>(ar1*(i-n1),ar2*j,ar3*k);
+		for (int j=0;j<n2/2+1;++j)
+		{
+			for (int i=0;i<n1/2+1;++i)
+				wv(i,j,k)=cat::tvector<Real,3>(ar1*i,ar2*j,ar3*k);
+			for (int i=n1/2+1;i<n1;++i)
+				wv(i,j,k)=cat::tvector<Real,3>(ar1*(i-n1),ar2*j,ar3*k);
+		}
+		for (int j=n2/2+1;j<n2;++j)
+		{
+			for (int i=0;i<n1/2+1;++i)
+				wv(i,j,k)=cat::tvector<Real,3>(ar1*i,ar2*(j-n2),ar3*k);
+			for (int i=n1/2+1;i<n1;++i)
+				wv(i,j,k)=cat::tvector<Real,3>(ar1*(i-n1),ar2*(j-n2),ar3*k);
+		}
 	}
-      for (int j=n2/2+1;j<n2;++j)
-	{
-	  for (int i=0;i<n1/2+1;++i)
-	    wv(i,j,k)=cat::tvector<Real,3>(ar1*i,ar2*(j-n2),ar3*k);
-	  for (int i=n1/2+1;i<n1;++i)
-	    wv(i,j,k)=cat::tvector<Real,3>(ar1*(i-n1),ar2*(j-n2),ar3*k);
-	}
-    }
-  
-      wv(0,0,0)=1e-30;
-
+	
+	wv(0,0,0)=1e-30;
+	
       //evaluate the square of norm of wavevectors
-      wv2=norm_sq(wv);
-
-      dealiasing_limit=4./9.*max(wv2);
-
-      //initialise dealiasing mask
-      cat::array_iterator<bool,3> dealiasing_mask_iterator(dealiasing_mask);
-      cat::array_const_iterator<Real,3> wv2_iterator(wv2);
-      for(dealiasing_mask_iterator=dealiasing_mask.begin(),
-	    wv2_iterator=wv2.begin();
-	  dealiasing_mask_iterator!=dealiasing_mask.end(),
-	    wv2_iterator!=wv2.end();
-	  ++dealiasing_mask_iterator,
-	    ++wv2_iterator)
-	if((*wv2_iterator)>dealiasing_limit)
-	  *dealiasing_mask_iterator=0;
-	else
-	  *dealiasing_mask_iterator=1;
-
+	wv2=norm_sq(wv);
+	
+	dealiasing_limit=4./9.*max(wv2);
+	
+  //initialise dealiasing mask
+	cat::array<bool,3>::iterator dm_iter(dealiasing_mask);
+	RSF::const_iterator wv2_iter(wv2);
+	for(dm_iter=dealiasing_mask.begin(),wv2_iter=wv2.begin();
+	    dm_iter!=dealiasing_mask.end(),wv2_iter!=wv2.end();
+	    ++dm_iter,++wv2_iter)
+	{
+		if((*wv2_iter)>dealiasing_limit)
+			(*dm_iter)=0;
+		else
+			(*dm_iter)=1;
+	}
 }
 
 
   //Desctructor
-  spectral_fourier::~spectral_fourier()
-    {
-    }
+SpectralFourier::~SpectralFourier()
+{
+}
 
 
-  //Dealiasing
-  //performs dealiasing for second order non-linearities
-  //scalar fields
-  void spectral_fourier::dealias(CSF& field) const
-  {
-    field*=dealiasing_mask;
-  }
-  //vector fields
-  void spectral_fourier::dealias(CVF& field) const
-  {
-    field*=dealiasing_mask;
-  }
-
-
-  //Solve lap(f)=g in fourier space
-  CSF spectral_fourier::poisson_hat(const CSF & field)
-  {
-    CSF out(field.shape());
-    out=field;
-    wv2(0,0,0)=1;
-    out/=(-wv2);
-    wv2(0,0,0)=1e-30;
-    return out;
-  }
-
-  //Solve lap(f)=g in fourier space
-  CVF spectral_fourier::poisson_hat(const CVF & field)
-  {
-    CVF out(field.shape());
-    out=field;
-    wv2(0,0,0)=1;
-    out/=(-wv2);
-    wv2(0,0,0)=1e-30;
-    return out;
-  }
-
-
-  //Derivatives in fourier space 
-
-
-  //Differential operators in fourier space
-  //deivatives in horizontal directions - scalars
-  CSF spectral_fourier::d_dx_index_hat(const CSF & field,const int index)
-  {
-    CSF out(field.shape());
-    out=field;
-    out*=wv[index];
-    out*=I;
-    return out;
-  }
-  //deivatives in horizontal directions - vectors
-  CVF spectral_fourier::d_dx_index_hat(const CVF & field, const int index)
-  {
-    CVF out;
-    out[0]=d_dx_index_hat(field[0],index);
-    out[1]=d_dx_index_hat(field[1],index);
-    out[2]=d_dx_index_hat(field[2],index);
-    return out;
-  }
-
+ //deivatives in respect to coordinate x_index - scalars
+CSF SpectralFourier::d_dx_index_hat(const CSF & field,const int index)
+{
+	CSF out(field.shape());
+	out=field;
+	out*=wv[index];
+	out*=I;
+	return out;
+}
+  //deivatives in respect to coordinate x_index - vectors
+CVF SpectralFourier::d_dx_index_hat(const CVF & field, const int index)
+{
+	CVF out;
+	out[0]=this->d_dx_index_hat(field[0],index);
+	out[1]=this->d_dx_index_hat(field[1],index);
+	out[2]=this->d_dx_index_hat(field[2],index);
+	return out;
+}
 
 
   //Gradient of scalar field
-  CVF spectral_fourier::grad_hat(const CSF & field)
-  {
-    CVF out(field.shape());
-    out=cat::tvector<Complex,3>(I,I,I);
-    out*=wv;
-    out*=field;
-    return out;
-  }
+CVF SpectralFourier::grad_hat(const CSF & field)
+{
+	CVF out(field.shape());
+	out=cat::tvector<Complex,3>(I,I,I);
+	out*=wv;
+	out*=field;
+	return out;
+}
   //Divergence of vector field
-  CSF spectral_fourier::div_hat(const CVF & field)
-  {
-    CVF outc(field.shape());
-    outc=cat::tvector<Complex,3>(I,I,I);
-    outc*=wv;
-    return CSF(dot_product(outc,field));
-  }
+CSF SpectralFourier::div_hat(const CVF & field)
+{
+	CVF outc(field.shape());
+	outc=cat::tvector<Complex,3>(I,I,I);
+	outc*=wv;
+	return CSF(dot_product(outc,field));
+}
 
   //Curl of vector field
-  CVF spectral_fourier::curl_hat(const CVF & field)
-  {
-    CVF outc(field.shape());
+CVF SpectralFourier::curl_hat(const CVF & field)
+{
+	CVF outc(field.shape());
     //Note that the z derivative acts on the 1st 2 components
-    outc=cat::tvector<Complex,3>(I,I,I);
-    outc*=wv;
-    return CVF(cross_product(outc,field));
-  }
+	outc=cat::tvector<Complex,3>(I,I,I);
+	outc*=wv;
+	return CVF(cross_product(outc,field));
+}
 
 
-  //Laplacian of scalar field
-  CSF spectral_fourier::lap_hat(const CSF & field)
-  {
-    CSF out(field.shape());
-    cat::array_iterator<Complex,3> out_iterator(out);
-    cat::array_const_iterator<Complex,3> field_iterator(field);
-    cat::array_const_iterator<Real,3> wv2_iterator(wv2);
-    for(out_iterator=out.begin(),
-	  field_iterator=field.begin(),
-	  wv2_iterator=wv2.begin();
-	out_iterator!=out.end(),
-	  field_iterator!=field.end(),
-	  wv2_iterator!=wv2.end();
-	++out_iterator,
-	  ++field_iterator,
-	  ++wv2_iterator)
-      {
-	(*out_iterator)=(*field_iterator);
-	(*out_iterator)*=(-(*wv2_iterator));
-      }
-    return out;
-  }
-  //Laplacian of vector field
-  CVF spectral_fourier::lap_hat(const CVF & field)
-  {
-    CVF out(field.shape());
-    cat::array_iterator<cat::tvector<Complex,3>,3> out_iterator(out);
-    cat::array_const_iterator<cat::tvector<Complex,3>,3> field_iterator(field);
-    cat::array_const_iterator<Real,3> wv2_iterator(wv2);
-    for(out_iterator=out.begin(),
-	  field_iterator=field.begin(),
-	  wv2_iterator=wv2.begin();
-	out_iterator!=out.end(),
-	  field_iterator!=field.end(),
-	  wv2_iterator!=wv2.end();
-	++out_iterator,
-	  ++field_iterator,
-	  ++wv2_iterator)
-      {
-	(*out_iterator)=(*field_iterator);
-	(*out_iterator)*=(-(*wv2_iterator));
-      }
-    return out;
-  }
 
-  //Extract gradient part 
+  //Extract gradient part
   //3D vector field
-  CVF spectral_fourier::remove_gradient(CVF & field)
-  {
-    CVF out(field.shape());
-    CSF sout(field.shape());
-    sout=div_hat(field);
-    wv2(0,0,0)=1;
-    sout/=(-wv2);
-    wv2(0,0,0)=1e-30;
-    out=grad_hat(sout);
-    field-=out;
-    return out;
-  }
+CVF SpectralFourier::remove_gradient(CVF & field)
+{
+	CVF out(field.shape());
+	CSF sout(field.shape());
+	sout=div_hat(field);
+	wv2(0,0,0)=1;
+	sout/=(-wv2);
+	wv2(0,0,0)=1e-30;
+	out=grad_hat(sout);
+	field-=out;
+	return out;
+}
 
 
 
   //L2 scalar product
   //scalars
-  Real spectral_fourier::scalar_prod(const CSF & x,
-				       const CSF & y) const
-  {
-    Real out=sum(real(x*conj(y)));
-    out*=2.;
-    for(int i=0;i<n1;++i)
-      for(int j=0;j<n2;++j)
- 	out-=(x(i,j,0)*conj(y(i,j,0))).real();
-    return out;
-  }
+Real SpectralFourier::scalar_prod(const CSF & x,
+                                  const CSF & y) const
+{
+	Real out=sum(real(x*conj(y)));
+	out*=2.;
+	for(int i=0;i<n1;++i)
+		for(int j=0;j<n2;++j)
+			out-=(x(i,j,0)*conj(y(i,j,0))).real();
+	return out;
+}
   //vectors
-  Real spectral_fourier::scalar_prod(const CVF & x,
-				       const CVF & y) const
-  {
-    return Real(scalar_prod(x[0],y[0])+
-		  scalar_prod(x[1],y[1])+
-		  scalar_prod(x[2],y[2]));
-  }
+Real SpectralFourier::scalar_prod(const CVF & x,
+                                  const CVF & y) const
+{
+	return Real(scalar_prod(x[0],y[0])+
+	            scalar_prod(x[1],y[1])+
+	            scalar_prod(x[2],y[2]));
+}
 
 
 //Evaluate energy spectrum
 //scalar fields
-cat::array<Real,1> spectral_fourier::eval_energ_spec(const CSF & field)
+cat::array<Real,1> SpectralFourier::eval_energ_spec(const CSF & field)
 {
-  int mx12=n1>n2?n1:n2;
-  int mx=mx12>n3?mx12:n3;
-  return eval_energ_spec(field,mx);
+	int mx12=n1>n2?n1:n2;
+	int mx=mx12>n3?mx12:n3;
+	return eval_energ_spec(field,mx);
 }
 //vector fields
-cat::array<Real,1> spectral_fourier::eval_energ_spec(const CVF & field)
+cat::array<Real,1> SpectralFourier::eval_energ_spec(const CVF & field)
 {
-  int mx12=n1>n2?n1:n2;
-  int mx=mx12>n3?mx12:n3;
-  return eval_energ_spec(field,mx);
+	int mx12=n1>n2?n1:n2;
+	int mx=mx12>n3?mx12:n3;
+	return eval_energ_spec(field,mx);
 }
 //Evaluate energy spectrum
 //dividing the sphere in npoints shells 
 //scalar fields
-cat::array<Real,1> spectral_fourier::eval_energ_spec(const CSF & field,const int & npoints)
+cat::array<Real,1> SpectralFourier::eval_energ_spec(const CSF & field,const int & npoints)
 {
-  cat::array<Real,1> out(npoints);
-  out=0;
-  Real wv2step=max(wv2)/(npoints-1);
-  cat::array_const_iterator<Complex,3> field_iterator(field);
-  cat::array_iterator<Real,3> wv2_iterator(wv2);
-  for(field_iterator=field.begin(),
-	wv2_iterator=wv2.begin();
-      field_iterator!=field.end(),
-	wv2_iterator!=wv2.end();
-      ++field_iterator,
-	++wv2_iterator)
-    out(static_cast<int>((*wv2_iterator)/wv2step))+=
-      ((*field_iterator)*conj(*field_iterator)).real();
-  return out;
+	cat::array<Real,1> out(npoints);
+	out=0;
+	Real wv2step=max(wv2)/(npoints-1);
+	CSF::const_iterator field_iterator(field);
+	RSF::iterator wv2_iterator(wv2);
+	for(field_iterator=field.begin(),
+	    wv2_iterator=wv2.begin();
+	    field_iterator!=field.end(),
+	    wv2_iterator!=wv2.end();
+	    ++field_iterator,
+	    ++wv2_iterator)
+		out(static_cast<int>((*wv2_iterator)/wv2step))+=
+		((*field_iterator)*conj(*field_iterator)).real();
+	return out;
 }
 //vector fields
-cat::array<Real,1> spectral_fourier::eval_energ_spec(const CVF & field,const int & npoints)
+cat::array<Real,1> SpectralFourier::eval_energ_spec(const CVF & field,const int & npoints)
 {
-  cat::array<Real,1> out(npoints);
-  out=0;
-  Real wv2step=max(wv2)/(npoints-1);
-  cat::array_const_iterator<cat::tvector<Complex,3>,3> field_iterator(field);
-  cat::array_iterator<Real,3> wv2_iterator(wv2);
-  for(field_iterator=field.begin(),
-	wv2_iterator=wv2.begin();
-      field_iterator!=field.end(),
-	wv2_iterator!=wv2.end();
-      ++field_iterator,
-	++wv2_iterator)
-    out(static_cast<int>((*wv2_iterator)/wv2step))+=
-      norm_sq(*field_iterator);
-  return out;
+	cat::array<Real,1> out(npoints);
+	out=0;
+	Real wv2step=max(wv2)/(npoints-1);
+	CVF::const_iterator field_iterator(field);
+	RSF::iterator wv2_iterator(wv2);
+	for(field_iterator=field.begin(),
+	    wv2_iterator=wv2.begin();
+	    field_iterator!=field.end(),
+	    wv2_iterator!=wv2.end();
+	    ++field_iterator,
+	    ++wv2_iterator)
+		out(static_cast<int>((*wv2_iterator)/wv2step))+=
+		norm_sq(*field_iterator);
+	return out;
 }
 
 
 //Printing non-vanishing harmonics in a field
-//scalar
-void spectral_fourier::pnvh(const CSF & field)
+
+void SpectralFourier::pnvh(const RSF & field)
 {
-    for(int i=0;i<field.shape()[0];i++)
-      for(int j=0;j<field.shape()[1];j++)
-	for(int k=0;k<field.shape()[2];k++)
-	  if(abs(field(i,j,k))>1e-10)
-	    cout << i << " " << j << " " << k << " " << setprecision(20) << field(i,j,k) << endl;
-  }
-  //vector
-  void spectral_fourier::pnvh(const CVF & field)
-  {
-    for(int i=0;i<field.shape()[0];i++)
-      for(int j=0;j<field.shape()[1];j++)
-	for(int k=0;k<field.shape()[2];k++)
-	  if(norm(field(i,j,k))>1e-10)
-	    cout << i << " " << j << " " << k << " " << setprecision(20) << field(i,j,k) << endl;
-  }
+	CSF field_hat(field.shape());
+	sfft.direct_transform(field_hat,field);
+	SpectralFourierBase::pnvh_hat(field_hat);
+}
 
-
-  void spectral_fourier::pnvh(const RSF & field)
-  {
-    CSF field_hat(field.shape());
-    sfft.direct_transform(field_hat,field);
-    pnvh(field_hat);
-  }
-
-  void spectral_fourier::pnvh(const RVF & field)
-  {
-    CVF field_hat(field.shape());
-    fft.direct_transform(field_hat,field);
-    pnvh(field_hat);  
-  }
+void SpectralFourier::pnvh(const RVF & field)
+{
+	CVF field_hat(field.shape());
+	fft.direct_transform(field_hat,field);
+	SpectralFourierBase::pnvh_hat(field_hat);
+}
--- spectral/spectral_fourier_layer.h
+++ spectral/spectral_fourier_layer.h
@@ -4,111 +4,93 @@
 #define SPECTRAL_FOURIER_LAYER_H
 
 #include "../goops_types.h"
-#include "../gffti/gffti.h"
+#include "../fft/fft.h"
+
+#include "spectral_fourier_base.h"
 
 #include <cat.h>
 
-class spectral_fourier_layer
+class SpectralFourierLayer : public SpectralFourierBase
 {
   //Members
+
 private:
-  //sizes
-  int n1,n2,n3;
-  Real l1,l2,l3;
-  
+	using SpectralFourierBase::n1;
+	using SpectralFourierBase::n2;
+	using SpectralFourierBase::n3;
+	using SpectralFourierBase::n1_hat;
+	using SpectralFourierBase::n2_hat;
+	using SpectralFourierBase::n3_hat;
+	using SpectralFourierBase::l1;
+	using SpectralFourierBase::l2;
+	using SpectralFourierBase::l3;
+	using SpectralFourierBase::ar1;
+	using SpectralFourierBase::ar2;
+	using SpectralFourierBase::ar3;
 public:
-  //wavevectors
-  RVF wv;
-    
-  //Square of norm of wavevectors 
-  RSF wv2;
-  
-private:
-  Real dealiasing_limit;
-  cat::array<bool,3> dealiasing_mask;
+	using SpectralFourierBase::wv;
+	using SpectralFourierBase::wv2;
 
 public:
   //Object to perform ffts
-  v_rfft_h_z_ccs fft_ccs;
-  v_rfft_h_z_ssc fft_ssc;
-  s_rfft_h_z<s_sinfft_1d> sfft_s;
-  s_rfft_h_z<s_cosfft_1d> sfft_c;
-  
-        
+  v_rfft_h_z fft_ccs;
+  v_rfft_h_z fft_ssc;
+  s_rfft_h_z sfft_s;
+  s_rfft_h_z sfft_c;
+      
   //Constructor and destructor
 public:
-  spectral_fourier_layer(const int & n1__,
+  SpectralFourierLayer(const int & n1__,
 			 const int & n2__,
 			 const int & n3__,
 			 const Real & l1__,
 			 const Real & l2__,
 			 const Real & l3__);
-  ~spectral_fourier_layer();
-      
+  ~SpectralFourierLayer();
+
+private:
+//Forbidden Ctors
+	SpectralFourierLayer();
+	SpectralFourierLayer(const SpectralFourierLayer &);
+	
+private:
+	void initialise();
+	
       //Public methods
   public:
   
-  //perform dialiasing on fields
-  void dealias(CSF & field) const;//scalar
-  void dealias(CVF & field) const;//vector
-  
-  
-  //Solve lap(f)=g in fourier space
-  CSF poisson_hat(const CSF & field);
-  CVF poisson_hat(const CVF & field);
-
-
-
- //Derivatives in fourier space
- 
-
-
-
-  //Differential operators in fourier space
-  //deivatives in horizontal directions - scalars
-  CSF d_dhorizontal_hat(const CSF & field,const int index);
+ //deivatives in horizontal directions - scalars
+  CSF d_dx_index_hat(const CSF & field,const int index);
   //deivatives in horizontal directions - vectors
-  CVF d_dhorizontal_hat(const CVF & field, const int index);
-  //Gradient of scalar field
+CVF d_dx_index_hat(const CVF & field, const int index);
+  
+//Gradient of scalar field
   CVF grad_hat(const CSF & field,const bool kind);
   //Divergence of vector field
   CSF div_hat(const CVF & field,const bool kind);
   //Curl of vector field
   CVF curl_hat(const CVF & field,const bool kind);
-  //Laplacian of scalar field
-  CSF lap_hat(const CSF & field);
-  //Laplacian of vector field
-  CVF lap_hat(const CVF & field);
- 
 
-  //remove gradient part after of a vfield
-  CVF remove_gradient(CVF & field,const bool kind);
-
-
+//Extract gradient part
+//3D vector field
+		CVF remove_gradient(CVF & field,const bool kind);
+	
   //L2 scalar product
-  Real scalar_prod(const CSF & x,
-		     const CSF & y,
-		     const bool kind) const;
-  Real scalar_prod(const CSF & x,
-		     const CSF & y) const;
-  Real scalar_prod(const CVF & x,
-		     const CVF & y) const;
-
+	Real scalar_prod(const CSF & x,const CSF & y,const bool kind) const;//scalar fields
+	Real scalar_prod(const CSF & x,const CSF & y) const;// To be moved to linops
+	Real scalar_prod(const CVF & x,const CVF & y,const bool kind) const;//vector fields
+	Real scalar_prod(const CVF & x,const CVF & y) const;// To be moved to linops
 
   //Energy spectrum
-  cat::array<Real,1> eval_energ_spec(const CSF & field);
-  cat::array<Real,1> eval_energ_spec(const CVF & field);
-  cat::array<Real,1> eval_energ_spec(const CSF & field,const int & npoints);
-  cat::array<Real,1> eval_energ_spec(const CVF & field,const int & npoints);
+	cat::array<Real,1> eval_energ_spec(const CSF & field,const bool & kind);//scalar fields
+	cat::array<Real,1> eval_energ_spec(const CVF & field,const bool & kind);//vector fields
+	cat::array<Real,1> eval_energ_spec(const CSF & field,const int & npoints,const bool & kind);//scalar fields
+	cat::array<Real,1> eval_energ_spec(const CVF & field,const int & npoints,const bool & kind);//vector fields
 
-
-
-  //print non-vanishing harmonics
-  void pnvh(const CVF & field);
-  void pnvh(const CSF & field);
-  
-  void pnvh(const RVF & field);
-  void pnvh(const RSF & field);
+  //print non-vanishing harmonics in real space
+	using SpectralFourierBase::pnvh_hat;
+	void pnvh(const RSF & field,const bool & kind);//scalar fields
+	void pnvh(const RVF & field,const bool & kind);//vector fields
   
 };
 
